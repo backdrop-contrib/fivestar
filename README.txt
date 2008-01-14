d89522d739bd06c5b613e50b8bdf967d50422b6b
Nathan Haug <quicksketch@35821.no-reply.drupal.org>
2008-01-14 06:24:24 +0000

    Updating HEAD with latest D5 copy for starting D6 port

--- README.txt
+++ README.txt
@@ -3,6 +3,7 @@
 The Five Star voting module adds a clean, attractive voting widget to nodes in Drupal 5. It features:
 
  * jQuery rollover effects and AJAX no-reload voting
+ * Customizable star sets
  * Graceful degradation to an HTML rating form when JavaScript is turned off
  * Per-nodetype configurability
  * Support for anonymous voters
@@ -11,4 +12,120 @@ The Five Star voting module adds a clean, attractive voting widget to nodes in D
  * A Fivestar CCK field for use in custom node types
  * An easy-to-use Form API element type for use in other modules
 
-Fivestar was designed by Nate Haug and Jeff Eaton of Lullabot.
+Fivestar was designed by Nate Haug and Jeff Eaton.
+
+This Module Made by Robots: http://www.lullabot.com
+
+
+Dependencies
+------------
+ * votingapi
+
+Fivestar also provides additional features for both the CCK and Views modules.
+
+Install
+-------
+Installing the Five Star voting module is simple:
+
+1) Copy the fivestar folder to the modules folder in your installation.
+
+2) Enable the module using Administer -> Modules (/admin/build/modules)
+
+Note: Aggressive caching will complain that fivestar doesn't work, but it
+actually causes no problems. To improve performance, the module implements
+hook_init() -- and the caching advisor screen uses that as the only metric to
+determine whether a module will work with the caching system. Activate it
+without fear, friends -- Fivestar will continue to hum happily along.
+
+Configuration for Simple Rating
+-------------------------------
+
+Fivestar has two completely separate modes of operation. The first is letting an
+end-user rate a piece of content. The settings for this are on the content type
+settings page. These settings let you expose a rating widget when viewing the
+node, not editing it. Clicking on the widget registers a vote for that node, and
+never anything else.
+
+The configuration for Fivestar is spread between the content type settings page,
+Fivestar site settings page, and access permissions. To configure:
+
+1) Configure the site-wide setting for Fivestar, Administer -> Settings ->
+   Fivestar.
+
+2) Activate voting on each content type. For example, if you want Fivestar to
+   appear on story nodes, use Administer -> Content Management ->
+   Content Types -> Story, and check the "Enable Five Star rating" box under
+   the "Five Star ratings" heading. Repeat for each content type desired.
+
+3) Enable anonymous voting.
+   If you want to allow anonymous voting, you'll need to set permissions for
+   that. Use Administer -> User Management -> Access Control, and check the
+   "rate content" and "view ratings" checkboxes for the roles you'd like.
+   You'll find these permission items under the "fivestar module" heading.
+
+Configuration as a CCK field / Advanced Rating
+----------------------------------------------
+
+Fivestar has extensive CCK support, which makes it so that the user is presented
+with a widget to rate some node with the widget while editing a node. It does
+not necessary rate the current node, nor does it rate anything if no value is
+entered in the Node ID field when configuring the CCK field. The value is
+saved in the node (so when you edit it it is still there), but no vote is
+registered in VotingAPI without the Node ID field filled out.
+
+An example of a situation where you might want to use the CCK fivestar field is
+creating a node type called 'review'. This review type would let users rate
+some particular node, and accompany their rating with a review. This could be
+combined with a standard rating on the target node, so that some users could
+rate the target node using the simple method, or write a complete review to
+accompany their rating.
+
+To configure a CCK field for rating a node while creating a new 'review' node:
+
+1) Create a new node type called 'review' at Administer -> Content Management ->
+Content Types. Configure the type. Do NOT set any fivestar settings on the
+content type form! We don't want users to actually be able to rate the reviews
+themselves!
+
+2) Edit your new content type, then click on the "Add Field" tab while on the
+content type form. Add a field called 'rating' to your new type, make it of type
+Fivestar Rating with the Stars radio button.
+
+3) Configure the rating widget to your liking. Most field have help text which
+explain their purpose. The Node ID field is the most important field on the page
+which determines exactly what node will receive the value of the rating. In a
+realy simple case, you could just enter the value 10 to always rate on the same
+node with nid = 10. Usually you'll need to enter PHP code to dynamically select
+what node you want to rate.
+
+A common scenario is using fivestar with nodecomments to make reviews. If using
+nodecomments a separate checkbox appears the Node ID field to allow you easily
+select the nodecomment parent as the target of the vote.
+
+Save your field. Now when making new nodes of type 'review', the user will
+select a star that will register a vote on the value of the Node ID field.
+
+Views Integration
+-----------------
+Fivestar depends on the views integration provided by VotinAPI, but adds some
+special features to make it work specifically with Fivestar. To display Fivestar
+ratings in a view, select the "VotingAPI percent vote result" from the list of
+available Fields. This will display the average vote for nodes. Then choose
+"Fivestar rating" from the Handler options for the field and the averages will
+be displayed as Fivestar ratings.
+
+Fivestar also provides handling for the display of Fivestar CCK fields, they are
+in the Field list under "Fivestar Rating: [Field name]".
+
+
+Contributing
+------------
+Have a sweet set of stars you'd like to contribute to the Fivestar module?
+Post them to the Fivestar issue queue: http://drupal.org/project/issues/fivestar
+
+Support
+-------
+If you experience a problem with fivestar or have a problem, file a
+request or issue on the fivestar queue at
+http://drupal.org/project/issues/fivestar. DO NOT POST IN THE FORUMS. Posting in
+the issue queues is a direct line of communication with the module authors.
--- jquery.rating.js
+++ jquery.rating.js
@@ -1,15 +1,14 @@
 /**
- * Star Rating - jQuery plugin
+ * Modified Star Rating - jQuery plugin
  *
  * Copyright (c) 2006 Wil Stuckey
  *
  * Original source available: http://sandbox.wilstuckey.com/jquery-ratings/
- * Extensively modified by Lullabot
+ * Extensively modified by Lullabot: http://www.lullabot.com
  *
  * Dual licensed under the MIT and GPL licenses:
  *   http://www.opensource.org/licenses/mit-license.php
  *   http://www.gnu.org/licenses/gpl.html
- *
  */
 
 /**
@@ -21,24 +20,58 @@
  * @type jQuery 
  *
  */
-(function($){ //create local scope
+(function($){ // Create local scope.
     /**
      * Takes the form element, builds the rating interface and attaches the proper events.
      * @param {Object} $obj
      */
     var buildRating = function($obj){
         var $widget = buildInterface($obj),
-            $stars = $($widget.children('.star')),
-            $cancel = $($widget.end().children('.cancel')),
-            averageIndex = 0,
-            averagePercent = 0;
-        // Set default rating
-        $obj.find("input[@type='radio']").each(function () { if (this.checked) { averageIndex = this.value; } }).end();
-        
-        $widget.end();
-        
-        // hover events.
-        // and focus events added
+            $stars = $('.star', $widget),
+            $cancel = $('.cancel', $widget),
+            $summary = $('.description', $obj).size() ? $('.description', $obj) : $($obj).siblings('.description'),
+            summaryText = $summary.html(),
+            summaryHover = $obj.is('.fivestar-labels-hover'),
+            currentValue = $("select", $obj).val(),
+            cancelTitle = $('label', $obj).html(),
+            voteTitle = cancelTitle != Drupal.settings.fivestar.titleAverage ? cancelTitle : Drupal.settings.fivestar.titleUser,
+            voteChanged = false;
+
+        // Record star display.
+        if ($obj.is('.fivestar-user-stars')) {
+          var starDisplay = 'user';
+        }
+        else if ($obj.is('.fivestar-average-stars')) {
+          var starDisplay = 'average';
+          currentValue = $("input[@name=vote_average]", $obj).val();
+        }
+        else if ($obj.is('.fivestar-combo-stars')) {
+          var starDisplay = 'combo';
+        }
+        else {
+          var starDisplay = 'none';
+        }
+
+        // Smart is intentionally separate, so the average will be set if necessary.
+        if ($obj.is('.fivestar-smart-stars')) {
+          var starDisplay = 'smart';
+        }
+
+        // Record text display.
+        if ($obj.is('.fivestar-user-text')) {
+          var textDisplay = 'user';
+        }
+        else if ($obj.is('.fivestar-average-text')) {
+          var textDisplay = 'average';
+        }
+        else if ($obj.is('.fivestar-combo-text')) {
+          var textDisplay = 'combo';
+        }
+        else {
+          var textDisplay = 'none';
+        }
+
+        // Add hover and focus events.
         $stars
             .mouseover(function(){
                 event.drain();
@@ -57,7 +90,7 @@
                 event.reset();
             });
         
-        // cancel button events
+        // Cancel button events.
         $cancel
             .mouseover(function(){
                 event.drain();
@@ -76,50 +109,123 @@
                 $(this).removeClass('on')
             });
         
-        // click events.
+        // Click events.
         $cancel.click(function(){
-            event.drain();
-            averageIndex = 0;
-            averagePercent = 0;
-            // Save the value in a hidden field
-            $obj.find("input[@type='radio']").each(function () { this.checked = (this.value ==  averageIndex) ? true : false; }).end();
-            // Submit the form if needed
-            $obj.find("input.fivestar-path").each(function () { $.get(this.value + '/' + averageIndex, null, voteHook); }).end();
+            currentValue = 0;
+            event.reset();
+            voteChanged = false;
+            // Save the currentValue in a hidden field.
+            $("select", $obj).val(0);
+            // Update the title.
+            cancelTitle = starDisplay != 'smart' ? cancelTitle : Drupal.settings.fivestar.titleAverage;
+            $('label', $obj).html(cancelTitle);
+            // Submit the form if needed.
+            $("input.fivestar-path", $obj).each(function () { $.ajax({ type: 'GET', dataType: 'xml', url: this.value + '/' + 0, success: voteHook }); });
             return false;
         });
         $stars.click(function(){
-            averageIndex = Math.ceil(($stars.index(this) + 1) * (100/$stars.size()));
-            averagePercent = 0;
-            // Save the value in a hidden field
-            $obj.find("input[@type='radio']").each(function () { this.checked = (this.value ==  averageIndex) ? true : false; }).end();
-            // Submit the form if needed
-            $obj.find("input.fivestar-path").each(function () { $.get(this.value + '/' + averageIndex, null, voteHook); }).end();
+            currentValue = $('select option', $obj).get($stars.index(this) + $cancel.size() + 1).value;
+            // Save the currentValue to the hidden select field.
+            $("select", $obj).val(currentValue);
+            // Update the display of the stars.
+            voteChanged = true;
+            event.reset();
+            // Submit the form if needed.
+            $("input.fivestar-path", $obj).each(function () { $.ajax({ type: 'GET', dataType: 'xml', url: this.value + '/' + currentValue, success: voteHook }); });
             return false;
         });
-        
+
         var event = {
-            fill: function(el){ // fill to the current mouse position.
-                var index = $stars.index(el) + 1;
-                $stars
-                    .children('a').css('width', '100%').end()
-                    .lt(index).addClass('hover').end();
+            fill: function(el){
+              // Fill to the current mouse position.
+              var index = $stars.index(el) + 1;
+              $stars
+                .children('a').css('width', '100%').end()
+                .filter(':lt(' + index + ')').addClass('hover').end();
+              // Update the description text and label.
+              if (summaryHover) {
+                var summary = $("select option", $obj)[index + $cancel.size()].text;
+                var value = $("select option", $obj)[index + $cancel.size()].value;
+                $summary.html(summary != index + 1 ? summary : '&nbsp;');
+                $('label', $obj).html(voteTitle);
+              }
             },
-            drain: function() { // drain all the stars.
-                $stars
-					.filter('.on').removeClass('on').end()
-					.filter('.hover').removeClass('hover').end();
-            },
-            reset: function(){ // Reset the stars to the default index.
-                $stars.lt(Math.floor(averageIndex/100 * $stars.size())).addClass('on').end();
-                var percent = (averagePercent) ? averagePercent * 10 : 0;
-                if (percent > 0) {
-                    $stars.eq(averageIndex).addClass('on').children('a').css('width', percent + "%").end().end()
+            drain: function() {
+              // Drain all the stars.
+              $stars
+                .filter('.on').removeClass('on').end()
+                .filter('.hover').removeClass('hover').end();
+              // Update the description text.
+              if (summaryHover) {
+                var cancelText = $("select option", $obj)[0].text;
+                $summary.html(($cancel.size() && cancelText != 0) ? cancelText : '&nbsp');
+                if (!voteChanged) {
+                  $('label', $obj).html(cancelTitle);
                 }
+              }
+            },
+            reset: function(){
+              // Reset the stars to the default index.
+              var starValue = currentValue/100 * $stars.size();
+              var percent = (starValue - Math.floor(starValue)) * 100;
+              $stars.filter(':lt(' + Math.floor(starValue) + ')').addClass('on').end();
+              if (percent > 0) {
+                $stars.eq(Math.floor(starValue)).addClass('on').children('a').css('width', percent + "%").end().end();
+              }
+              // Restore the summary text and original title.
+              if (summaryHover) {
+                $summary.html(summaryText ? summaryText : '&nbsp;');
+              }
+              if (voteChanged) {
+                $('label', $obj).html(voteTitle);
+              }
+              else {
+                $('label', $obj).html(cancelTitle);
+              }
             }
-        }        
+        };
+
+        /**
+         * Checks for the presence of a javascript hook 'fivestarResult' to be
+         * called upon completion of a AJAX vote request.
+         */
+        var voteHook = function(data) {
+          var returnObj = {
+            result: {
+              count: $("result > count", data).text(),
+              average: $("result > average", data).text(),
+              summary: { average: $("summary average", data).text(), user: $("summary user", data).text(), combo: $("summary combo", data).text() }
+            },
+            vote: {
+              id: $("vote id", data).text(),
+              type: $("vote type", data).text(),
+              value: $("vote value", data).text()
+            },
+            display: {
+              stars: starDisplay,
+              text: textDisplay
+            }
+          };
+          // Check for a custom callback.
+          if (window.fivestarResult) {
+            fivestarResult(returnObj);
+          }
+          // Use the default.
+          else {
+            fivestarDefaultResult(returnObj);
+          }
+          // Update the summary text.
+          summaryText = returnObj.result.summary[returnObj.display.text];
+          // Update the current star currentValue to the previous average.
+          if (returnObj.vote.value == 0 && starDisplay == 'average') {
+            currentValue = returnObj.result.average;
+            event.reset();
+          }
+        };
+
         event.reset();
         return $widget;
-    }
+    };
     
     /**
      * Accepts jQuery object containing a single fivestar widget.
@@ -130,59 +236,85 @@
      * 
      */
     var buildInterface = function($widget){
-        var $container = $(document.createElement('div')).attr({
-            "class": 'fivestar-widget clear-block'
-        });
-        var $radios = $widget.find("input[@type='radio']");
-        var size = $radios.size() - 1;
+        var $container = $('<div class="fivestar-widget clear-block"></div>');
+        var $options = $("select option", $widget);
+        var size = $('option', $widget).size() - 1;
         var cancel = 1;
-        for (var i = 0, radio; radio = $radios[i]; i++){
-            if (radio.value == "0") {
+        for (var i = 1, option; option = $options[i]; i++){
+            if (option.value == "0") {
               cancel = 0;
-              $div = $('<div class="cancel"><a href="#0" title="Cancel Rating">Cancel Rating</a></div>');
+              $div = $('<div class="cancel"><a href="#0" title="' + option.text + '">' + option.text + '</a></div>');
             }
             else {
-              $div = $('<div class="star"><a href="#' + radio.value + '" title="Give it ' + (i + cancel) + '/'+ (size + cancel) +'">' + radio.value + '</a></div>');
+              var zebra = (i + cancel - 1) % 2 == 0 ? 'even' : 'odd';
+              var count = i + cancel - 1;
+              var first = count == 1 ? ' star-first' : '';
+              var last = count == size + cancel - 1 ? ' star-last' : '';
+              $div = $('<div class="star star-' + count + ' star-' + zebra + first + last + '"><a href="#' + option.value + '" title="' + option.text + '">' + option.text + '</a></div>');
             }
             $container.append($div[0]);                    
         }
-        // Attach the new widget and hide the existing widget
-        $widget.end().hide().after($container);
+        $container.addClass('fivestar-widget-' + (size + cancel - 1));
+        // Attach the new widget and hide the existing widget.
+        $('select', $widget).after($container).hide();
         return $container;
-    }
-    
+    };
+
     /**
-     * Checks for the presence of a javascript hook 'fivestarResult' to be called upon completion of a AJAX vote request
+     * Standard handler to update the average rating when a user changes their
+     * vote. This behavior can be overridden by implementing a fivestarResult
+     * function in your own module or theme.
+     * @param object voteResult
+     * Object containing the following properties from the vote result:
+     * voteResult.result.count The current number of votes for this item.
+     * voteResult.result.average The current average of all votes for this item.
+     * voteResult.result.summary.average The textual description of the average.
+     * voteResult.result.summary.user The textual description of the user's current vote.
+     * voteResult.vote.id The id of the item the vote was placed on (such as the nid)
+     * voteResult.vote.type The type of the item the vote was placed on (such as 'node')
+     * voteResult.vote.average The average of the new vote saved
+     * voteResult.display.stars The type of star display we're using. Either 'average', 'user', or 'combo'.
+     * voteResult.display.text The type of text display we're using. Either 'average', 'user', or 'combo'.
      */
-     var voteHook = function(data) {
-       var returnObj = new Object();
-       returnObj.result = new Object();
-       returnObj.vote = new Object();
-       returnObj.result.count = $("count",$(data)).text();
-       returnObj.result.average = $("average",$(data)).text();
-       returnObj.vote.id = $("id",$(data)).text();
-       returnObj.vote.type = $("type",$(data)).text();
-       returnObj.vote.value = $("value",$(data)).text();
-       
-       // Look for the fivestarResult function and call if available
-       if (window.fivestarResult) {
-         fivestarResult(returnObj);
-       }
-     }
-    
+    function fivestarDefaultResult(voteResult) {
+      // Update the summary text.
+      $('div.fivestar-summary-'+voteResult.vote.id).html(voteResult.result.summary[voteResult.display.text]);
+      // If this is a combo display, update the average star display.
+      if (voteResult.display.stars == 'combo') {
+        $('div.fivestar-form-'+voteResult.vote.id).each(function() {
+          var $stars = $('.fivestar-widget-static .star span', this);
+          var average = voteResult.result.average/100 * $stars.size();
+          var index = Math.floor(average);
+          $stars.removeClass('on').addClass('off').css('width', 'auto');
+          $stars.filter(':lt(' + (index + 1) + ')').removeClass('off').addClass('on');
+          $stars.eq(index).css('width', ((average - index) * 100) + "%");
+        });
+      }
+    };
+
     /**
      * Set up the plugin
      */
-    $.fn.rating = function(){
-        var stack = [];
-        this.each(function(){
-            var ret = buildRating($(this));
-            stack = $.merge(ret, stack);
-        });
-        return $(stack);
-    }
-	// fix ie6 background flicker problem.
-	if ($.browser.msie == true) {
-		document.execCommand('BackgroundImageCache', false, true);
-	}
-})(jQuery)
+    $.fn.rating = function() {
+      var stack = [];
+      this.each(function() {
+          var ret = buildRating($(this));
+          stack.push(ret);
+      });
+      return stack;
+    };
+
+  // Fix ie6 background flicker problem.
+  if ($.browser.msie == true) {
+    try {
+      document.execCommand('BackgroundImageCache', false, true);
+    } catch(err) {}
+  }
+})(jQuery);
+
+if (Drupal.jsEnabled) {
+  $(document).ready(function() {
+    $('div.fivestar-form-item').rating();
+    $('input.fivestar-submit').hide();
+  });
+}
